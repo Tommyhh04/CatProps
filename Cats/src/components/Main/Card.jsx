1f7d911f6b5fe7ebd3906a06458a881d29765abf
Tommyhh04 <tommyhh04.kodehode@gmail.com>
2023-12-14 11:01:43 +0100

    Added images

--- Cats/src/components/Main/Card.jsx
+++ Cats/src/components/Main/Card.jsx
@@ -1,24 +1,40 @@
 import "./styleCard.css";
 import CatCard from "./ContactCard/CatCards";
 
+//Imports profile images
+import Cat_Luna from "../Images/Luna.jpg";
+import Cat_Milo from "../Images/Milo.jpg";
+import Cat_Grafild from "../Images/Grafild.jpg";
+import Cat_Bella from "../Images/Bella.jpg";
 /*Contains the cards to be displayed here*/
 
 export default function Main() {
   return (
     <div className="container--main">
       <CatCard
+        img={Cat_Luna}
         name="Luna"
         email="luna@catmail.com"
         phone="+1 (555) 123-4567"
-        img="../Images/Luna.jpg"
       />
-      <CatCard name="Milo" email="milo@meow.com" phone="+1 (555) 234-5678" />
       <CatCard
-        name="Oliver"
-        email="oliver@purrfect.com"
+        img={Cat_Milo}
+        name="Milo"
+        email="milo@meow.com"
+        phone="+1 (555) 234-5678"
+      />
+      <CatCard
+        img={Cat_Grafild}
+        name="Grafild"
+        email="Grafild@pasta.com"
         phone="+1 (555) 345-6789"
       />
-      <CatCard name="Bella" email="bella@kitty.com" phone="+1 (555) 456-7890" />
+      <CatCard
+        img={Cat_Bella}
+        name="Bella"
+        email="bella@kitty.com"
+        phone="+1 (555) 456-7890"
+      />
     </div>
   );
 }
--- Cats/src/components/Main/ContactCard/CatCards.jsx
+++ Cats/src/components/Main/ContactCard/CatCards.jsx
@@ -4,8 +4,13 @@ import "./StyleCatCard.css";
 export default function CatCard(props) {
   return (
     <div className="contact--card">
-      <img src={props.img} alt="Profile image for cat" />
+      <img
+        className="Profile--Img"
+        src={props.img}
+        alt="Profile image for cat"
+      />
       <h1>{props.name}</h1>
+      <p>{props.desc}</p>
       <h3>{props.phone}</h3>
       <h4>{props.email}</h4>
     </div>
